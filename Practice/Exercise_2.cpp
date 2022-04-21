6479c20f053d1268095cbe879ffd84da26b0d4ec
STRIXBG <65187330+STRIXBG@users.noreply.github.com>
2022-04-21 09:42:54 +0300

    Update Exercise_2.cpp

--- Practice/Exercise_2.cpp
+++ Practice/Exercise_2.cpp
@@ -7,14 +7,14 @@ int main() {
 	setlocale(LC_ALL, "Bulgarian");
 	int numGrades;
 	double averageGrade;
-	cout << "Âúâåäè áðîÿ îöåíêè: "; cin >> numGrades;
+	cout << "Enter count of grades: "; cin >> numGrades;
 	for(int i=0; i<numGrades; i++){
-		cout << "Âúâåäè áðîÿ îöåíêè: ";
+		cout << "Enter grade: ";
 		int grade;
 		cin >> grade;
 		averageGrade += grade;	
 	}
 	averageGrade /= numGrades;
-	cout << "Ñðåäíèÿò óñïåõ: "; cin >> averageGrade;
+	cout << "Average Grade: " << setprecision(2) << averageGrade;
 	return 0;
 }
